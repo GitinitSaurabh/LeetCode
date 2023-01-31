d25c28ca592a1b71ea56e281e72c822274af8b93
Saurabh <saurabh160196@gmail.com>
2023-01-31 19:58:45 +0530

    Keys and rooms BFS GRAPH solution

--- CPP/KeysAndRooms.cpp
+++ CPP/KeysAndRooms.cpp
@@ -5,22 +5,32 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         vector<bool> visited(rooms.size(), false);
-        dfs(0, rooms, visited);
+        queue<int> q;
+        bfs(0, rooms, visited,q);
         for(int i=0; i<visited.size(); i++){
             if(!visited[i]){
                 return false;
             }
         }
         return true;
+        
     }
 public:
-    void dfs(int node, vector<vector<int>>& rooms, vector<bool>& visited){
+    void bfs(int node, vector<vector<int>>& rooms, vector<bool>& visited, queue<int>& q){
 
         visited[node] = true;
-        for(auto neighbor : rooms[node]){ 
+        q.push(node);
+        while(!q.empty()){
+            int currNode = q.front();
+            q.pop();
+            for(auto neighbor : rooms[currNode]){ 
             if(!visited[neighbor]){
-                dfs(neighbor, rooms, visited);
+                q.push(neighbor);
+                visited[neighbor] = true;
+                // q.push(neighbor);
+                }
             }
         }
+        
     }
 };
