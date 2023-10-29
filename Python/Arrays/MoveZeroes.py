aee84cf1e37a08b0e0aa66a87b728b66caff1601
Saurabh <saurabh160196@gmail.com>
2023-10-30 03:20:58 +0530

    move all zeroes to right side of same array using two pointer approach

---
+++ Python/Arrays/MoveZeroes.py
@@ -0,0 +1,16 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i = 0
+        j = 0
+
+        for i in range(len(nums)):
+            if(nums[i] != 0):
+                nums[j] = nums[i]
+                j += 1
+        
+        for i in range (j, len(nums)):
+            nums[i] = 0
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
