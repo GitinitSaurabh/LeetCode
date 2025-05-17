a92c4407f9d8ceb2df9fe44f82bf0bd495e61bed
Saurabh A <saurabh160196@gmail.com>
2025-05-18 03:29:13 +0530

    ZeroMatrix Additional In-Space O(1) solution

--- Python/Arrays/ZeroMatrix.py
+++ Python/Arrays/ZeroMatrix.py
@@ -2,25 +2,36 @@ class Solution:
     def ZeroMatrix(self, matrix: list[list[int]]) -> None:
         rows = len(matrix)
         cols = len(matrix[0])
-        zeroRows: set[int] = set()
-        zeroCols: set[int] = set()
-
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j] == 0:
-                    zeroRows.add(i)
-                    zeroCols.add(j)
         
-        for i in range(rows):
-            for j in range(cols):
-                if i in zeroRows or j in zeroCols:
+        firstRowZero = any(matrix[0][j] == 0 for j in range(cols))
+        firstColZero = any(matrix[i][0] == 0 for i in range(rows))
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
+
+        if firstRowZero:
+            for j in range(cols):
+                matrix[0][j] = 0
+        
+        if firstColZero:
+            for i in range(rows):
+                matrix[i][0] = 0  
+        
 
 sol = Solution()
 matrix = [
-    [1, 2, 3],
-    [4, 0, 6],
-    [7, 8, 9]
+    [5,  1,  9, 11],
+    [2,  0,  6,  8],
+    [15, 7,  10, 12],
+    [0, 14, 13, 16]
 ]
 
 sol.ZeroMatrix(matrix)
